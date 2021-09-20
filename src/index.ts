ced6fc57fccece4f3b1c209cd422efc993455159
Felipe Santos <felipecassiors@gmail.com>
2021-09-20 11:23:34 -0300

    Better organize the code

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,15 @@
-import { Application } from "typedoc";
-import { Converter, Context } from "typedoc";
-import { Reflection } from "typedoc";
+import { Application, Context, Converter, Reflection } from "typedoc";
 
 export function load(app: Readonly<Application>) {
   app.converter.on(
     Converter.EVENT_CREATE_DECLARATION,
     (_context: Context, reflection: Reflection, node?: any) => {
-      if (!node || !node.name || (reflection.name !== "default" && reflection.name !== "export=")) return;
+      if (
+        !node ||
+        !node.name ||
+        (reflection.name !== "default" && reflection.name !== "export=")
+      )
+        return;
 
       reflection.name = node.name.getText();
     }
